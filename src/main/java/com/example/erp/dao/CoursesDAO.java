fdfbe42bb39cd0737bb0c4800b00fff4c8412c02
Abhi <Abhinav.Singh@iiitb.org>
2020-12-25 22:36:47 +0530

    first commit

--- src/main/java/com/example/erp/dao/CoursesDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.erp.dao;
-
-import com.example.erp.bean.Courses;
-
-public interface CoursesDAO {
-    public abstract void addCourse(Courses course);
-}
--- src/main/java/com/example/erp/dao/StudentsDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.erp.dao;
-
-import com.example.erp.bean.Students;
-
-public interface StudentsDAO {
-    public abstract void addStudent(Students student);
-}
